c573b41daf3e1a7ea5f777e19888baa5660a1bd6
Peter Kyeyune <peterkayk@gmail.com>
2025-01-31 14:14:18 +0300

    Update build artifacts and server configuration for new deployment

--- server.ts
+++ server.ts
@@ -1,6 +1,12 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+// Purpose: This file is used to create a server for the application. This file is used to create a server for the application.
+
+const {next} = require('next');
+const { createServer } = require('http');
+const { parse } = require('url');
+
+import { IncomingMessage, ServerResponse } from 'http';
+import { UrlWithParsedQuery } from 'url';
+
  
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,10 +14,14 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
  
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+interface ParsedRequest extends IncomingMessage {
+    url: string;
+}
+
+createServer((req: ParsedRequest, res: ServerResponse) => {
+    const parsedUrl: UrlWithParsedQuery = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+}).listen(port);
  
   console.log(
     `> Server listening at http://localhost:${port} as ${
